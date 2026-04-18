059ad01319db1ea862778d2d42b1725fd3e12f5d
RajCodes <rajarshiyadav1@gmail.com>
2026-04-18 15:05:17 +0530

    updated new

--- vision_assist/build.sh
+++ vision_assist/build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "==> Installing system dependencies..."
-apt-get update -qq
-apt-get install -y tesseract-ocr tesseract-ocr-eng libgl1 libglib2.0-0
-
 echo "==> Installing Python dependencies..."
 pip install -r requirements.txt
 
